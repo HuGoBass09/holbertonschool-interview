277ecab980d6b03979d6dda5f777080595e9aabf
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-06-28 23:04:31 +0400

    lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and,
+each box may contain keys to the other boxes.
+"""
+
+def canUnlockAll(boxes):
+    unlockedBoxes = set([0])
+    keys = list(boxes[0])
+    
+    while keys:
+        key = keys.pop()
+        if key not in unlockedBoxes:
+            unlockedBoxes.add(key)
+            keys.extend(boxes[key])
+    
+    return len(unlockedBoxes) == len(boxes)
