81015cc495224c4177b6d80166d71edd78b66f5c
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-07-23 23:17:50 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -5,7 +5,6 @@
 /**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
- *
  */
 static void print_grid(int grid[3][3])
 {
@@ -24,69 +23,78 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * make_stable - make a grid stable if it is unstable
+ * copy_grid - Copy a 3x3 grid to another
+ * @src: Source grid
+ * @dest: Destination grid
+ */
+void copy_grid(int src[3][3], int dest[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			dest[i][j] = src[i][j];
+		}
+	}
+}
+
+/**
+ * stabilize_grid - Check and stabilize a grid
  * @grid: 3x3 grid
+ * @temp: Temporary 3x3 grid for stabilization
  *
+ * Return: true if the grid was unstable, false otherwise
+ */
+bool stabilize_grid(int grid[3][3], int temp[3][3])
+{
+	int i, j;
+	bool isUnstable = false;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				isUnstable = true;
+				temp[i][j] -= 4;
+
+				if (i > 0)
+					temp[i - 1][j] += 1;
+				if (i < 2)
+					temp[i + 1][j] += 1;
+				if (j > 0)
+					temp[i][j - 1] += 1;
+				if (j < 2)
+					temp[i][j + 1] += 1;
+			}
+		}
+	}
+
+	return isUnstable;
+}
+
+/**
+ * make_stable - Make a grid stable if it is unstable
+ * @grid: 3x3 grid
  */
 void make_stable(int grid[3][3])
 {
-	int i, j;
+	int temp[3][3];
 	bool isUnstable = true;
 
 	while (isUnstable)
 	{
-		int temp[3][3];
-		isUnstable = false;
-
-		for (i = 0; i < 3; ++i)
-		{
-			for (j = 0; j < 3; ++j)
-			{
-				temp[i][j] = grid[i][j];
-			}
-		}
-
-		for (i = 0; i < 3; ++i)
-		{
-			for (j = 0; j < 3; ++j)
-			{
-				if (grid[i][j] > 3)
-				{
-					isUnstable = true;
-					temp[i][j] -= 4;
-
-					if (i > 0)
-					{
-						temp[i - 1][j] += 1;
-					}
-					if (i < 2)
-					{
-						temp[i + 1][j] += 1;
-					}
-					if (j > 0)
-					{
-						temp[i][j - 1] += 1;
-					}
-					if (j < 2)
-					{
-						temp[i][j + 1] += 1;
-					}
-				}
-			}
-		}
+		copy_grid(grid, temp);
+		isUnstable = stabilize_grid(grid, temp);
 
 		if (isUnstable)
 		{
 			printf("=\n");
 			print_grid(grid);
-
-			for (i = 0; i < 3; ++i)
-			{
-				for (j = 0; j < 3; ++j)
-				{
-					grid[i][j] = temp[i][j];
-				}
-			}
+			copy_grid(temp, grid);
 		}
 	}
 }
@@ -95,17 +103,16 @@ void make_stable(int grid[3][3])
  * sandpiles_sum - Computes the sum of two sandpiles
  * @grid1: 3x3 grid
  * @grid2: 3x3 grid
- *
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 
-	for (i = 0; i < 3; ++i)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 3; ++j)
+		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
+			grid1[i][j] += grid2[i][j];
 		}
 	}
 
