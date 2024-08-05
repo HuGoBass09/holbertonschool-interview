4c15b40076e99fac7ea292a132dc6e2658731bef
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-05 14:06:06 +0400

    log_parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -23,20 +23,23 @@ status_codes = {
 
 
 def print_statistics(statuses, total):
-    print("File size: {}".format(total))
+    print(f"File size: {total}")
     for key, value in sorted(statuses.items()):
         if value != 0:
-            print("{}: {}".format(key, value))
+            print(f"{key}: {value}")
 
 
-for line in sys.stdin:
-    new_line = line.rstrip().split(" ")
-    if len(new_line) == 9 or len(new_line) == 7:
-        logs += 1
-        total_size += int(new_line[-1])
-        status_codes[new_line[-2]] += 1
-        if logs % 10 == 0 and logs != 0:
-            print_statistics(status_codes, total_size)
-
-
-print_statistics(status_codes, total_size)
+try:
+    for line in sys.stdin:
+        new_line = line.rstrip().split(" ")
+        if len(new_line) == 9 or len(new_line) == 7:
+            try:
+                logs += 1
+                total_size += int(new_line[-1])
+                status_codes[new_line[-2]] += 1
+                if logs % 10 == 0 and logs != 0:
+                    print_statistics(status_codes, total_size)
+            except BaseException:
+                pass
+finally:
+    print_statistics(status_codes, total_size)
