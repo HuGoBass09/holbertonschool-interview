7ef9e6887a1e9f162f0675f9007f1703e96f9c84
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-14 20:07:48 +0400

    log_parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -6,31 +6,43 @@ all info will be printed"""
 
 import sys
 
-file_size = 0
-count = 0
-ids = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+
+def main():
+    file_size = 0
+    count = 0
+    ids = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+
+    def print_msg(ids, file_size):
+        print("File size: {}".format(file_size))
+        for key, val in sorted(ids.items()):
+            if val != 0:
+                print("{}: {}".format(key, val))
+
+    try:
+        for line in sys.stdin:
+            nums = line.rstrip().split(" ")
+            try:
+                if nums[-2] in ids:
+                    ids[nums[-2]] += 1
+                file_size += int(nums[-1])
+                count += 1
+                if count % 10 == 0:
+                    print_msg(ids, file_size)
+            except BaseException:
+                pass
+
+    finally:
+        print_msg(ids, file_size)
 
 
-def print_msg(ids, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(ids.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-try:
-    for line in sys.stdin:
-        nums = line.rstrip().split(" ")
-        try:
-            if nums[-2] in ids:
-                ids[nums[-2]] += 1
-            file_size += int(nums[-1])
-            count += 1
-            if count % 10 == 0:
-                print_msg(ids, file_size)
-        except BaseException:
-            pass
-
-
-finally:
-    print_msg(ids, file_size)
+if __name__ == "__main__":
+    main()
