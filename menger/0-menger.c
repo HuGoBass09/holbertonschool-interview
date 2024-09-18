9e214053079112f93d000f9bcbe927c65fbc98d7
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-18 19:41:50 +0400

    betty_style

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,33 +1,39 @@
-#include "menger.h"
+#include <stdio.h>
+#include <math.h>
+
 /**
  * menger - function that draws a 2D Menger Sponge
- * @level: level of the menger sponge to draw
+ * @level: level of the Menger sponge to draw
  */
-
 void menger(int level)
 {
-    int n, m, j, x, y;
-    char s;
+  int n;
+  int m;
+  int j;
+  int x;
+  int y;
+  char s;
 
-    m = pow(3, level);
-    for (n = 0; n < m; n++)
+  m = (int)pow(3, level);
+  for (n = 0; n < m; n++)
+  {
+    for (j = 0; j < m;)
     {
-        for (j = 0; j < m;)
+      s = '#';
+      x = n;
+      y = j++;
+      while (x > 0 || y > 0)
+      {
+        if (x % 3 == 1 && y % 3 == 1)
         {
-            s = '#';
-            x = n;
-            y = j++;
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    s = ' ';
-                }
-                x /= 3;
-                y /= 3;
-            }
-            printf("%c", s);
+          s = ' ';
+          break;
         }
-        printf("\n");
+        x /= 3;
+        y /= 3;
+      }
+      printf("%c", s);
     }
+    printf("\n");
+  }
 }
